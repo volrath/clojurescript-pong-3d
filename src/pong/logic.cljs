a5ed6ba7a110bbf54976535db5110d839cbb9aca
Daniel Barreto <dbarreto@talpor.com>
2016-06-05 09:58:01 -0400

    Play a bit with velocity alteration.
    Renames `:ball :velocity` to `:ball :direction`, since it should be a
    normalized direction vector. `:speed` scalar is tracked on its own.

--- src/pong/logic.cljs
+++ src/pong/logic.cljs
@@ -4,6 +4,8 @@
             [pong.game-objects :refer [paddle-depth paddle-height paddle-width]]))
 
 (def paddle-speed 2)
+(def ball-min-speed 0.5)
+(def ball-max-speed 2)
 
 (defn -paddle-x [pos]
   (let [border-dist (/ (:width field-size) 2)]
@@ -13,13 +15,15 @@
 
 (defn -create-paddle [side]
   {:position {:x (-paddle-x side) :y 0 :z paddle-depth}
+   :direction nil
    :side side
    :collide false})
 
 (defn -create-ball [pointing-to]
   (let [vx (if (= pointing-to :player-1) -1 1)]
     {:position {:x 0 :y 0 :z 0}
-     :velocity {:x vx :y (rand-nth [1 -1])}
+     :direction {:x vx :y (rand-nth [1 -1])}  ; velocity's direction
+     :speed 1
      :hit false}))
 
 ;;
@@ -45,7 +49,9 @@
 
 (defn -move-paddle [paddle direction]
   (let [op (if (= direction :up) + -)]
-    (update-in paddle [:position :y] op paddle-speed)))
+    (-> paddle
+        (assoc :direction direction)
+        (update-in [:position :y] op paddle-speed))))
 
 (defn -paddle-within-boundaries [{:keys [y]}]
   (let [half-boundary (* (:height field-size) 0.45)]
@@ -58,7 +64,7 @@
       (if (-paddle-within-boundaries (:position new-paddle))
         new-paddle
         (-alert-invalid-move-for-paddle paddle)))
-    paddle))
+    (assoc paddle :direction nil)))
 
 ;;
 ;; Ball Physics
@@ -70,43 +76,61 @@
 (defn -reset-ball []
   (-create-ball (first @scores)))
 
-(defn -boundary-collision-velocity-alteration [{{y :y} :position :as ball}]
+(defn -boundary-collision-direction-alteration [{{y :y} :position :as ball}]
   "If the ball collides with the world boundaries (y-axis), vy gets inverted"
   (let [half-boundary (/ (:height field-size) 2)]
     (if (or (<= y (- half-boundary))
             (>= y half-boundary))
-      (update-in ball [:velocity :y] -)
+      (update-in ball [:direction :y] -)
       ball)))
 
+(defn -slice-ball-speed
+  [{{dy :y} :direction speed :speed :as ball}  ; ball
+   {:keys [direction]}]                        ; paddle
+  "Returns a new speed scalar when the ball hits the given paddle. If the ball
+  and the paddle were going on the same y-axis direction, speed gets increased,
+  if not, it gets decreased. This method checks speed don't go off a
+  `max-speed`-`min-speed` boundaries."
+  (if direction
+    (let [ndy (/ dy (js/Math.abs dy))  ; normalized vy
+          paddle-scalar-direction (if (= direction :up) 1 -1)
+          alteration (* ndy paddle-scalar-direction 0.3)]
+      (update ball :speed #(-> %
+                               (+ alteration)
+                               (min ball-max-speed)
+                               (max ball-min-speed))))
+    ball))
+
 (defn -paddle-collision-velocity-alteration
-  [{{bx :x by :y} :position ball-velocity :velocity :as ball}
-   {{px :x py :y} :position side :side}]
-  "If the ball collides with paddles, vx gets inverted and vy gets slightly
-  modified"
+  [{{bpx :x bpy :y} :position {bdx :x} :direction :as ball}
+   {{ppx :x ppy :y} :position side :side :as paddle}]
+  "If the ball collides with paddles, vx gets inverted. If the ball was sliced,
+  speed gets altered and vy's angle might change slightly."
   (let [op (if (= side :left) < >)
-        ball-towards-paddle (op (:x ball-velocity) 0)
+        ball-towards-paddle (op bdx 0)
         half-paddle-height (/ paddle-height 2)]
     (if (and ball-towards-paddle
-             (<= bx (+ px paddle-width))
-             (>= bx px)                            ; Hitting paddle in x-axis
-             (<= by (+ py half-paddle-height))
-             (>= by (- py half-paddle-height)))    ; Ball and paddle vertically aligned y-axis
+             (<= bpx (+ ppx paddle-width))
+             (>= bpx ppx)                            ; Hitting paddle in x-axis
+             (<= bpy (+ ppy half-paddle-height))
+             (>= bpy (- ppy half-paddle-height)))    ; Ball and paddle vertically aligned y-axis
       (-> ball
           (assoc :hit true)
-          (update-in [:velocity :x] -))            ; TODO: make y more aggresive
+          (update-in [:direction :x] -)
+          (-slice-ball-speed paddle))
       ball)))
 
 (defn -alter-velocity [ball paddle-1 paddle-2]
   "Returns a new ball with its velocity altered"
   (-> ball
-      -boundary-collision-velocity-alteration
+      -boundary-collision-direction-alteration
       (-paddle-collision-velocity-alteration paddle-1)
       (-paddle-collision-velocity-alteration paddle-2)))
 
 (defn -update-ball [ball paddle-1 paddle-2]
-  (let [{{ball-x :x} :position} ball
+  (let [{{ball-x :x} :position speed :speed} ball
         update-position (fn [b axis]
-                          (update-in b [:position axis] + (-> b :velocity axis)))]
+                          (update-in b [:position axis] + (-> b :direction axis (* speed))))]
     (if (-scored? ball-x)
       (-reset-ball)
       (-> ball
