fbc85f0bcac7b36e588506db40d4ded70042ff3b
Daniel Barreto <dbarreto@talpor.com>
2016-06-05 23:09:37 -0400

    Make HTML give a better gameplay idea.

--- src/pong/components.cljs
+++ src/pong/components.cljs
@@ -1,15 +1,25 @@
 (ns pong.components
   (:require [pong.defs :refer [up-to-wins]]
-            [pong.logic :refer [player-score scores]]
+            [pong.logic :refer [game-over? player-score scores]]
             [rum.core :as rum]))
 
+(defn declare-winner [p1-score p2-score]
+  (let [winner-text {p1-score "You win!" p2-score "CPU wins!"}]
+    (get winner-text (max p1-score p2-score))))
+
 (rum/defc -score-board < rum/reactive []
-  (let [p1-score (player-score :player-1 (rum/react scores))
-        p2-score (player-score :player-2 (rum/react scores))]
+  (let [scores (rum/react scores)
+        p1-score (player-score :player-1 scores)
+        p2-score (player-score :player-2 scores)]
     [:div#score-board
-     [:h1#scores (str p1-score " - " p2-score)]
+     [:h1#scores (if (game-over?)
+                   (declare-winner p1-score p2-score)
+                   (str p1-score " - " p2-score))]
      [:h1#title "- 3D Pong -"]
-     [:h2#winner-board (str "First to " up-to-wins " wins!")]]))
+     [:h2#winner-board (if (game-over?)
+                         "Refresh to play again"
+                         (str "First to " up-to-wins " wins!"))]
+     [:h3#controls "A - move left" [:br] "D - move right"]]))
 
 (def mount-scene
   {:did-mount (fn [state]
